1fc7e5c9c59e63f373293d3cd6c900ae073ab124
Josue Gallegos <gallegosjosue6@gmail.com>
2023-08-04 14:50:47 -0500

    add search input focus event listener
    start by console logging when focus event fires

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 console.log("main.js connected");
 
+const handleFormInputFocus = () => {
+  console.log(`focus occured`);
+};
+
 const SearchTermsInput = document.body.querySelector("#search-terms");
+
+SearchTermsInput.addEventListener("focus", handleFormInputFocus);
