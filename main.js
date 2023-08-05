4403e30eafd936930399f233adc0b88cece7fe84
Josue Gallegos <gallegosjosue6@gmail.com>
2023-08-05 14:35:23 -0500

    fetch returns data and creates container

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 console.log("main.js connected");
 
 const searchTermsInput = document.body.querySelector('#search-terms');
+const cardForm = document.querySelector('#card-form');
+const resultsContainer = document.querySelector('#results-container');
 
-const getCardData = async () => {
+const getCardData = async (name) => {
         const cardDataApiURL = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
         try {
-            const response = await fetch(cardDataApiURL)
-            const data = response.json();
-            console.log(`data: `, data)
+            const response = await fetch(cardDataApiURL);
+            const data = await response.json();
+            const limitedCards = data.data.slice(0,15);
+            return data.data;
         } catch (error) {
             console.log(error)
-            alert('something went wrong')
+            alert('Something went wrong, please try again later.')
         }
-        
-        
-}
+
+};
+
+const displayCards = (cards) => {
+    while (resultsContainer.firstChild) {
+
+    }
+    
+    cards.forEach(card => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
 
         const cardName = document.createElement('h3');
         cardName.textContent = card.name;
