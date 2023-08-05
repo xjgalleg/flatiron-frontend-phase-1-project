44b3bded5ff827870cdc223202141169c5101542
Josue Gallegos <gallegosjosue6@gmail.com>
2023-08-04 19:11:51 -0500

    search terms input, API fetch, handle form input focus updated

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 console.log("main.js connected");
 
+const searchTermsInput = document.body.querySelector('#search-terms');
+
+const getCardData = async () => {
         const cardDataApiURL = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+        try {
+            const response = await fetch(cardDataApiURL)
+            const data = response.json();
+            console.log(`data: `, data)
+        } catch (error) {
+            console.log(error)
+            alert('something went wrong')
+        }
+        
+        
+}
+
+const handleFormInputFocus = async () => {
   console.log(`focus occured`);
+
+ await getCardData()
 };
 
-const SearchTermsInput = document.body.querySelector("#search-terms");
-
-SearchTermsInput.addEventListener("focus", handleFormInputFocus);
+searchTermsInput.addEventListener("focus", handleFormInputFocus);
