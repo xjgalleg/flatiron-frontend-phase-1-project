405ffb3cdcb3bd501967beba0e6257f70fe81785
Josue Gallegos <gallegosjosue6@gmail.com>
2023-08-06 04:56:38 -0500

    small adjustments to code in js file

--- main.js
+++ main.js
@@ -1,60 +1,61 @@
 console.log("main.js connected");
 
 document.addEventListener("DOMContentLoaded", () => {
-const cardForm = document.querySelector('#card-form');
-const resultsContainer = document.querySelector('#results-container');
-const deckContainer = document.querySelector('#deck-container');
-const cardInfoBox = document.querySelector('#card-info-box');
+    const searchTermsInput = document.querySelector('#search-terms');
+    const cardForm = document.querySelector('#card-form');
+    const resultsContainer = document.querySelector('#results-container');
+    const deckContainer = document.querySelector('#deck-container');
+    const cardInfoBox = document.querySelector('#card-info-box');
 
-let deck = [];
+    let deck = [];
 
-const getCardData = async (fuzzyName) => {
-        const cardDataApiURL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+    const getCardData = async (name) => {
+        const cardDataApiURL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${name}`;
         try {
             const response = await fetch(cardDataApiURL);
             const data = await response.json();
             return data.data;
         } catch (error) {
-            console.log(error)
-            alert('Something went wrong, please try again later.')
+            console.log(error);
+            alert('Something went wrong, please try again later.');
         }
-};
+    };
 
-const displayCards = (cards) => {
-    while (resultsContainer.firstChild) {
-        resultsContainer.removeChild(resultsContainer.firstChild);
-    }
-    
-    cards.forEach(card => {
-        const cardDiv = document.createElement('div');
-        cardDiv.classList.add('card');
+    const displayCards = (cards) => {
+        while (resultsContainer.firstChild) {
+            resultsContainer.removeChild(resultsContainer.firstChild);
+        }
+        
+        cards.forEach(card => {
+            const cardDiv = document.createElement('div');
+            cardDiv.classList.add('card');
 
             const cardName = document.createElement('h4');
-        cardName.textContent = card.name;
-        cardDiv.appendChild(cardName);
+            cardName.textContent = card.name;
+            cardDiv.appendChild(cardName);
 
-        cardDiv.addEventListener('click', () => {
+            cardDiv.addEventListener('click', () => {
                 if (deck.includes(card)) {
-            displayCardInfo(card);
+                    displayCardInfo(card);
                 } else {
                     addCardToDeck(card);
                 }
+            });
+            
+            resultsContainer.appendChild(cardDiv);
         });
+    };
 
-        resultsContainer.appendChild(cardDiv);
-    });
-};
+    const addCardToDeck = (card) => {
+        if (deck.length < 40) {
+            deck.push(card);
+            updateDeck();
+        } else {
+            alert("Your deck is full!");
+        }
+    };
 
-const addCardToDeck = (card) => {
-    if (deck.length < 40) {
-        deck.push(card);
-        updateDeck();
-    } else {
-        alert("Your deck is full!");
-    }
-};
-
-const updateDeck = () => {
+    const updateDeck = () => {
         deckContainer.innerText = ''; 
 
         const deckList = document.createElement('ul');
@@ -63,7 +64,7 @@ const updateDeck = () => {
             const listItem = document.createElement('li');
             listItem.textContent = `${index + 1}. ${card.name}`;
             deckList.appendChild(listItem);
-         });
+        });
         deckContainer.appendChild(deckList);
     };
 
@@ -71,37 +72,42 @@ const updateDeck = () => {
         deck = deck.filter(c => c !== card);
         updateDeck();
         clearCardInfo();
-};
+    };
 
-const displayCardInfo = (card) => {
-    cardInfoBox.innerHTML = '';
+    const displayCardInfo = (card) => {
+        cardInfoBox.innerText = '';
 
-    const cardInfoDiv = document.createElement('div');
-    cardInfoDiv.classList.add('card-info-box');
+        const cardInfoDiv = document.createElement('div');
+        cardInfoDiv.classList.add('card-info-box');
 
-    const cardName = document.createElement('h3');
-    
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove from Deck';
-    removeButton.addEventListener('click', () => {
-        removeCardFromDeck(card);
-    });
+        const cardName = document.createElement('h3');
+        cardName.textContent = card.name;
 
-    cardInfoDiv.appendChild(cardName);
-    cardInfoDiv.appendChild(removeButton);
-    cardInfoDiv.appendChild(cardInfoDiv);
-};
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove from Deck';
+        removeButton.addEventListener('click', () => {
+            removeCardFromDeck(card);
+        });
 
+        cardInfoDiv.appendChild(cardName);
+        cardInfoDiv.appendChild(removeButton);
 
-const handleFormSubmit = async (event) => {
-    event.preventDefault();
+        cardInfoBox.appendChild(cardInfoDiv);
+    };
 
-    const searchTerm = searchTermsInput.value.trim();
-    if (searchTerm !== '') {
-        const cards = await getCardData(searchTerm);
-        displayCards(cards);
-    }
-};
+    const clearCardInfo = () => {
+        cardInfoBox.innerText = '';
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const searchTerm = searchTermsInput.value.trim();
+        if (searchTerm !== '') {
+            const cards = await getCardData(searchTerm);
+            displayCards(cards);
+        }
+    };
 
     deckContainer.addEventListener('click', (event) => {
         const clickedListItem = event.target.closest('li');
@@ -115,4 +121,5 @@ const handleFormSubmit = async (event) => {
             }
         }
     });
-cardForm.addEventListener("submit", handleFormSubmit);
+    cardForm.addEventListener("submit", handleFormSubmit);
+});
